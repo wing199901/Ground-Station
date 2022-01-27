b773755aae112b001ca73e85f4fef57808510923
Steven Siu <wing199901@gmail.com>
2022-01-27 11:33:12 +0800

    update api to RESTful style

--- API/api.py
+++ API/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 import mysql_operate as mysql
 from mqtt_client import MyMQTTClass
@@ -11,38 +11,87 @@ app.config["DEBUG"] = True
 CORS(app)
 
 # mqtt client dict
-clients = {}
+mqtt_clients = {}
 
 
-# register new device
-@app.route('/register_device', methods=['POST'])
-def new_device():
-    device = str(request.args.get('device_id'))
-    print(device)
-    print(len(device))
+@app.route('/')
+def index():
+    return "Online"
 
-# device id must be 3 letters or 4 letters
-    if len(device) != 3 and len(device) != 4:
-        return "Not a propre device id."
 
-    name = "CONSOLE40-" + device if len(device) > 3 else "MODELA-" + device
+# get all devices
+@app.route('/api/devices', methods=['GET'])
+def get_all_devices():
+    # get data from database
+    sql = f"SELECT id FROM device;"
+    data = mysql.db.select_db(sql)
 
-    sql = f"SELECT * FROM device WHERE id = '{device}';"
+    return jsonify({'devices': data})
+
+
+# get a device
+@app.route('/api/devices/<string:device_id>', methods=['GET'])
+def get_device(device_id):
+    # device id must be 3 letters or 4 letters
+    if len(device_id) != 3 and len(device_id) != 4:
+        return make_response(jsonify({'error': f'{device_id} not a propre device id.'}), 404)
+
+    # get data from database
+    sql = f"SELECT * FROM device WHERE id = '{device_id}';"
     data = mysql.db.select_db(sql)
 
     if data:
-        return "Already exists"
+        return jsonify({device_id: data})
     else:
-        sql = "INSERT INTO device values('" + device + "','" + name + "');"
+        return make_response(jsonify({'error': f'Device id {device_id} does not exist.'}), 404)
+
+
+# create new device
+@app.route('/api/devices/<string:device_id>', methods=['POST'])
+def create_device(device_id):
+    # device id must be 3 letters or 4 letters
+    if len(device_id) != 3 and len(device_id) != 4:
+        return make_response(jsonify({'error': f'{device_id} not a propre device id.'}), 404)
+
+    # full name of the device
+    name = "CONSOLE40-" + \
+        device_id if len(device_id) > 3 else "MODELA-" + device_id
+
+    # check if the record already exists
+    sql = f"SELECT * FROM device WHERE id = '{device_id}';"
+    data = mysql.db.select_db(sql)
+
+    if data:
+        return make_response(jsonify({'error': f'Device id {device_id} already exists.'}), 404)
+    else:
+        sql = "INSERT INTO device values('" + device_id + "','" + name + "');"
         mysql.db.execute_db(sql)
-        return ""
+        return "", 201
 
 
-# get new session id
-@app.route('/new_session', methods=['POST'])
-def new_session():
-    sql = "INSERT INTO session(startTime) VALUES (CURRENT_TIMESTAMP)"
+# get all session
+@app.route('/api/sessions', methods=['GET'])
+def get_all_session():
+    sql = f"SELECT * FROM session;"
+    data = mysql.db.select_db(sql)
+    return jsonify({"sessions": data})
 
+
+# get session details
+@app.route('/api/sessions/<int:session_id>', methods=['GET'])
+def get_all_devices_by_session_id(session_id):
+    sql = f"SELECT device.id, device.name FROM device INNER JOIN session_device on device.id = session_device.device_id where session_device.session_id = {session_id};"
+    data = mysql.db.select_db(sql)
+
+    if data:
+        return jsonify({"session": data})
+    else:
+        return make_response(jsonify({'error': f'Session id {session_id} does not exist.'}), 404)
+
+
+# create session
+@app.route('/api/sessions', methods=['POST'])
+def create_session():
     # sql = """
     # START TRANSACTION;
     # INSERT INTO session(StartTime) VALUES (CURRENT_TIMESTAMP);
@@ -50,103 +99,119 @@ def new_session():
     # COMMIT;
     # """
 
+    sql = "INSERT INTO session(startTime) VALUES (CURRENT_TIMESTAMP)"
     mysql.db.execute_db(sql)
     # https://www.python.org/dev/peps/pep-0249/#lastrowid
     data = mysql.db.cursor.lastrowid
-    data = {"session_id": data}
-    return jsonify(data)
+    return jsonify({"session_id": data}), 201
 
 
 # add device to session
-@app.route('/add_device', methods=['POST'])
-def add_device_to_session():
-    device = str(request.args.get('device_id'))
-
-    session = str(request.args.get('session_id'))
-
+@app.route('/api/sessions/<int:session_id>/<string:device_id>', methods=['POST'])
+def add_device_to_session(session_id, device_id):
     # check session exist
-    sql = f"SELECT id FROM session WHERE id = '{session}';"
+    sql = f"SELECT id FROM session WHERE id = '{session_id}';"
     data = mysql.db.select_db(sql)
 
     if not data:
-        return "Wrong session id"
+        return make_response(jsonify({'error': f'Session id {session_id} does not exist.'}), 404)
+
+    # device id must be 3 letters or 4 letters
+    if len(device_id) != 3 and len(device_id) != 4:
+        return make_response(jsonify({'error': f'{device_id} not a propre device id.'}), 404)
 
     # check device exist
-    sql = f"SELECT id FROM device WHERE id = '{device}';"
+    sql = f"SELECT id FROM device WHERE id = '{device_id}';"
     data = mysql.db.select_db(sql)
 
     if not data:
-        return "Wrong device id"
+        return make_response(jsonify({'error': f'Device id {device_id} does not exist.'}), 404)
 
-    sql = f"INSERT INTO session_device VALUES('{session}','{device}');"
+    sql = f"INSERT INTO session_device VALUES('{session_id}','{device_id}');"
     mysql.db.execute_db(sql)
-    return ""
+    return "", 201
 
 
-# query device by session id
-@app.route('/query', methods=['POST'])
-def get_all_devices_by_session_id():
-
-    session = str(request.args.get('session_id'))
-
-    sql = f"SELECT device.id, device.name FROM device INNER JOIN session_device on device.id = session_device.device_id where session_device.session_id = {session};"
+# query all flight data in session
+@app.route('/api/flight_data/<int:session_id>', methods=['GET'])
+def get_flight_data(session_id):
+    # check session exist
+    sql = f"SELECT id FROM session WHERE id = '{session_id}';"
     data = mysql.db.select_db(sql)
-    data = {"devices": data}
+
+    if not data:
+        return make_response(jsonify({'error': f'Session id {session_id} does not exist.'}), 404)
+
+    sql = f"SELECT * FROM data WHERE session_id = {session_id};"
+    data = mysql.db.select_db(sql)
+    data = {'flight_data': data}
     return jsonify(data)
+
+
+# query flight data in session from a device
+@app.route('/api/flight_data/<int:session_id>/<string:device_id>', methods=['GET'])
+def get_device_flight_data(session_id, device_id):
+    # check session exist
+    sql = f"SELECT id FROM session WHERE id = '{session_id}';"
+    data = mysql.db.select_db(sql)
+
+    if not data:
+        return make_response(jsonify({'error': f'Session id {session_id} does not exist.'}), 404)
+
+    # device id must be 3 letters or 4 letters
+    if len(device_id) != 3 and len(device_id) != 4:
+        return make_response(jsonify({'error': f'{device_id} not a propre device id.'}), 404)
+
+    # check device exist
+    sql = f"SELECT id FROM device WHERE id = '{device_id}';"
+    data = mysql.db.select_db(sql)
+
+    if not data:
+        return make_response(jsonify({'error': f'Device id {device_id} does not exist.'}), 404)
+
+    sql = f"SELECT * FROM data WHERE session_id = {session_id} AND device_id = '{device_id}';"
+    data = mysql.db.select_db(sql)
+
+    if data:
+        return jsonify({device_id: data})
+    else:
+        return make_response(jsonify({'error': 'Flight data does not exist.'}), 404)
 
 
 # trigger MQTT client to start recording flight data
-@app.route('/start_session', methods=['POST'])
-def start_record():
-    session = str(request.args.get('session_id'))
-
-    client = MyMQTTClass(session_id=session)
+@app.route('/api/start_session/<int:session_id>', methods=['POST'])
+def start_recording(session_id):
+    client = MyMQTTClass(session_id=session_id)
     client.run()
 
     # add client to clients dict
-    clients[session] = client
-    return ""
+    mqtt_clients[session_id] = client
+    return "", 200
 
 
 # trigger MQTT client to stop record flight data
-@app.route('/stop_session', methods=['POST'])
-def stop_record():
-    session = str(request.args.get('session_id'))
+@app.route('/api/stop_session/<int:session_id>', methods=['POST'])
+def stop_record(session_id):
+    # check session exist
+    sql = f"SELECT id FROM session WHERE id = '{session_id}';"
+    data = mysql.db.select_db(sql)
+
+    if not data:
+        return make_response(jsonify({'error': f'Session id {session_id} does not exist.'}), 404)
 
     try:
-        client = clients[session]
+        client = mqtt_clients[session_id]
         client.stop()
-        del clients[session]
-    except:
-        return "Session not exists"
-    return ""
+        del mqtt_clients[session_id]
+    except Exception as error:
+        return make_response(jsonify({'error': f'{error}'}), 417)
+
+    return "", 200
 
 
-# query flight data by session id
-@app.route('/query_flight_data', methods=['POST'])
-def get_all_flight_data_by_session_id():
-    device = str(request.args.get('device_id'))
-
-    session = str(request.args.get('session_id'))
-
-    # check session exist
-    sql = f"SELECT id FROM session WHERE id = '{session}';"
-    data = mysql.db.select_db(sql)
-
-    if not data:
-        return "Wrong session id"
-
-    # check device exist
-    sql = f"SELECT id FROM device WHERE id = '{device}';"
-    data = mysql.db.select_db(sql)
-
-    if not data:
-        return "Wrong device id"
-
-    sql = f"SELECT * FROM data WHERE session_id = {session} AND device_id = '{device}';"
-    data = mysql.db.select_db(sql)
-    data = {"flight_record": data}
-    return jsonify(data)
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': error}), 404)
 
 
 if __name__ == '__main__':
--- API/mysql_config.py
+++ API/mysql_config.py
@@ -2,4 +2,4 @@ MYSQL_HOST = "aerosim.synology.me"
 MYSQL_PORT = 3307
 MYSQL_USER = "onetomany"
 MYSQL_PASSWD = "Stp@erosim2022"
-MYSQL_DB = "flight_record"
+MYSQL_DB = "flight_data"
